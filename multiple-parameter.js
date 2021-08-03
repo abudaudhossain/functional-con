6ecb8ab5618b6829c7d6958048d3747f50d4f782
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-03 10:53:13 +0600

    add multiple paramiter

---
+++ multiple-parameter.js
@@ -0,0 +1,22 @@
+// add two number 
+function addTwoNumbers(number1, number2) {
+    return number1 + number2;
+}
+
+// multiplication two numbers  
+function multiplyTwoNumbers(number1, number2) {
+    return number1 * number2;
+}
+
+// difference two Numbers 
+function diffeTwoNumber(number1, number2) {
+    return number1 - number2;
+}
+
+var sum = addTwoNumbers(12, 34);
+var multipply = multiplyTwoNumbers(12, 34);
+var Defferent = diffeTwoNumber(12, 34);
+
+console.log(sum);
+console.log(multipply);
+console.log(Defferent);
