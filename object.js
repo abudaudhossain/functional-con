d6a78ea3ed5bd6ddd8bf07a90a2f9efcc285f007
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-03 11:06:38 +0600

    add object

---
+++ object.js
@@ -0,0 +1,19 @@
+var student = {
+    name: "Abu Daud",
+    age: 23,
+    phone: 232554654323,
+    tecnology: 'Computer'
+}
+
+var mobile = {
+    color: 'Black',
+    brand: 'Nokia',
+    price: 3000,
+    size: 4
+}
+var book = {
+    title: "Start With Why",
+    author: "Simon Sinek",
+    price: 200,
+    pages: 245
+}
